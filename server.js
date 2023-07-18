5b01148445df462ab9d4e22ef4a33d921e3d03fd
Karissa Lange <112651523+ka-lange@users.noreply.github.com>
2023-07-18 13:54:43 -0400

    add root routes to server

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
 const connectDB = require('./config/database')
+const homeRoutes = require('./routes/home')
+const habitRoutes = require('./routes/habits')
 
 require('dotenv').config({path: './config/.env'})
 
 connectDB()  //initialize connecting the server to the database via config/database file
+
+app.set('view engine', 'ejs')
+// app.use(express.static('public'))
+// app.use(express.urlencoded({ extended: true }))
+// app.use(express.json())
+
+app.use('/', homeRoutes)
+app.use('/habits', habitRoutes)
 
 app.listen(process.env.PORT, ()=>{
     console.log('Server is running, you better catch it!')
