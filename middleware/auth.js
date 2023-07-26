2f9eb021037f52ad399a0f0d7e6e94925bb95436
Karissa Lange <112651523+ka-lange@users.noreply.github.com>
2023-07-26 10:37:15 -0400

    first attempt to add user authentication

---
+++ middleware/auth.js
@@ -0,0 +1,10 @@
+// module.exports = {
+//     ensureAuth: function (req, res, next) {
+//       if (req.isAuthenticated()) { //checks to see if you are authenticated
+//         return next() //if you are, good, you can go to next page
+//       } else {
+//         res.redirect('/') //if not, no not good, takes back to home login page
+//       }
+//     }
+//   }
+  
