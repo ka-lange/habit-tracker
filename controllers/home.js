a913c5fc2407be0c46de0a87acf85b0185c5cc4e
Karissa Lange <112651523+ka-lange@users.noreply.github.com>
2023-07-25 23:04:03 -0400

    remove dark styling by index route

--- controllers/home.js
+++ controllers/home.js
@@ -12,15 +12,6 @@ module.exports = {
             console.log(err)
         }
     },
-    getDarkmodeIndex: async (req,res)=>{
-        // res.render('index.ejs') //renders ejs file and reponds with it
-        try{
-            const Habits = await Habit.find()//uses variable Todo model to grab todo instances from database, mongoose automatically turns to array
-            // const itemsLeft = await Todo.countDocuments({completed: false})
-            res.render('darkmodeindex.ejs', {habits: Habits})
-        }catch(err){
-            console.log(err)
-        }
-    }
+    
 }
 
--- routes/home.js
+++ routes/home.js
@@ -3,5 +3,5 @@ const router = express.Router()
 const homeController = require('../controllers/home')
 
 router.get('/', homeController.getIndex) //uses home controller to get index page and render index.js from views
-router.get('/darkmodeindex', homeController.getDarkmodeIndex)
+
 module.exports = router
