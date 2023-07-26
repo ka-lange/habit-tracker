d3e18369309b7b804d8be44bb3a9882f90a8c2cd
Karissa Lange <112651523+ka-lange@users.noreply.github.com>
2023-07-26 17:48:41 -0400

    add static file server

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ connectDB()  //initialize connecting the server to the database via config/datab
 
 app.set('view engine', 'ejs')
 app.use('/public', express.static('public'))
+app.use(express.static(__dirname + '/public'));
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
